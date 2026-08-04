08de7094e29d618e4079b2bffe51b817811bcf93
altafnazir <altafnpatel@gmail.com>
2026-08-04 20:24:24 +0530

    Remove uncecessary files

--- AzureArchitect/src/AzurePubSub/Abstractions/IPublisher.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace AzureArchitect.Abstractions
-{
-    public interface IPublisher
-    {
-        Task PublishAsync(string topicName, object payload, System.Collections.Generic.IDictionary<string, string>? properties = null);
-    }
-}
